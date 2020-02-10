151acf9e96fc4db009ffaf1bdbfa79808802396d
Giovanni Lodi <giovanni.lodi42@gmail.com>
2020-02-10 12:32:24 +1100

    Update Dangerfile to make inline comments

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,4 +3,16 @@
 
 import {warn, danger} from "danger";
 
-warn("This demo warning was generated on " + new Date());
+const readme = danger
+  .git
+  .created_files
+  .concat(danger.git.deleted_files)
+  .concat(danger.git.modified_files)
+  .forEach (path => {
+    // I couldn't find a way to read the diff because the DSL function to do so
+    // is async and I don't know how to make this Dangerfile await for it.
+    //
+    // Let's just make a silly inline comment here, rather than something
+    // smarter based on the diff content
+    warn("Demo inline comment made at " + new Date(), path, 0)
+  })
